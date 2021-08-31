b50ed3162ef67d9f937b27f9ad184bf19ea46ed8
neslihanince <neslihanince94@gmail.com>
2021-08-31 07:25:17 -0400

    Day 01: firefox / starter

---
+++ src/test/java/com/cybertek/day01_navigation_locators/BasicNavigation_FireFox.java
@@ -0,0 +1,18 @@
+package com.cybertek.day01_navigation_locators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+public class BasicNavigation_FireFox {
+
+    public static void main(String[] args) {
+
+        //set up FireFox driver
+        WebDriverManager.firefoxdriver().setup();
+
+        //create Webdriver instance using FirefoxDriver object
+        WebDriver 
+
+        //navigate to "https://cybertekschool.com
+    }
+}
