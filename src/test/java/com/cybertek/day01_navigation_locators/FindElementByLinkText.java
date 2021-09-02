e81e5b5ba05b37d09b115f03def25a32596a3998
neslihanince <neslihanince94@gmail.com>
2021-09-02 07:20:09 -0400

    Day 01: findElement By name practice find element by link text starter

---
+++ src/test/java/com/cybertek/day01_navigation_locators/FindElementByLinkText.java
@@ -0,0 +1,27 @@
+package com.cybertek.day01_navigation_locators;
+
+public class FindElementByLinkText {
+
+    public static void main(String[] args) {
+
+        // set up chrome driver, open chrome
+
+        // navigate to https://google.com
+
+        //try to identify search box using both name and id
+
+        //submit the search clicking on the search button
+
+        //navigate back to google home page
+
+        // wait few second
+
+        // click on about link
+
+        //wait few seconds
+
+        //close browser
+
+
+    }
+}
