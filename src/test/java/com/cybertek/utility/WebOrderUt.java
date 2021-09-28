20a32857bdf91c829c4da233779616db6d090928
neslihanince <neslihanince94@gmail.com>
2021-09-28 14:35:50 -0400

    HomeWork1: Create new `WebOrderUtility` with different name or modify existing one.

---
+++ src/test/java/com/cybertek/utility/WebOrderUt.java
@@ -0,0 +1,16 @@
+package com.cybertek.utility;
+
+public class WebOrderUt {
+
+    /**
+     * Simply navigate to WebOrderApp
+     */
+
+    public static void openWebOrderApp(){
+
+        Driver.getDriver();
+    }
+
+
+
+}
